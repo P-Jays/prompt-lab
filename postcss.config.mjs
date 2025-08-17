9fa24b1e9830a6bb913a62d886d74bbda0c73e52
PJ <goldprajnawi@gmail.com>
2025-08-17 17:57:50 +0800

    init

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,17 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
+    },
+  },
 };
+
 
 export default config;
