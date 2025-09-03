c713c9c7502ca4045a37394d688737cbcba72b74
PJ <goldprajnawi@gmail.com>
2025-09-03 16:22:13 +0800

    adding search bar

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent, useRef } from "react";
 import PromptCard from "./PromptCard";
 import { Prompt } from "@/types/types";
 
@@ -29,7 +29,10 @@ const PromptCardList: React.FC<PromptCardListProps> = ({
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e: unknown) => {};
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const searchTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [searchedResults, setSearchedResults] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -37,9 +40,39 @@ const Feed = () => {
       const data = await response.json();
 
       setPosts(data);
-    }
+    };
     fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext: string) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item: Prompt) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (searchTimeoutRef.current) clearTimeout(searchTimeoutRef.current);
+
+    const value = e.target.value;
+    setSearchText(value);
+
+    searchTimeoutRef.current = setTimeout(() => {
+      const searchResult = filterPrompts(value);
+      setSearchedResults(searchResult);
+    }, 500);
+  };
+
+  const handleTagClick = (tagName: string) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form action="" className="relative w-full flex-center">
@@ -51,7 +84,14 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}}></PromptCardList>
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
